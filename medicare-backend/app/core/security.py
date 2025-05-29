f1e8454080d2f82306a2115bf569adeae6263e68
Ronaldo Ottoni Borges de Carvalho <ronaldo@softdib.com.br>
2025-05-29 13:35:29 -0300

    JWT Security

---
+++ medicare-backend/app/core/security.py
@@ -0,0 +1,29 @@
+from passlib.context import CryptContext
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+import os
+
+SECRET_KEY =os.getenv("JWT_SECRET", "supersecret")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+pwd_context = CryptContext(schemas=["bcrypt"], deprecated="auto")
+
+def gerar_hash_senha(senha: str):
+    return pwd_context.hash(senha)
+
+def verificar_senha(senha: str, hash:str):
+    return pwd_context.verify(senha, hash)
+
+def criar_token(data: dict):
+    to_encode = data.copy()
+    expira = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expira})
+    return jwt.encode(to_encode, SECRET_KEY, algorithms=ALGORITHM)
+
+def verificar_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        return payload
+    except JWTError:
+        return None
